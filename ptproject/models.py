fe1add6e86968f312907a484239b1c304ce5d5a6
helenahagman <helena@rapibo.se>
2024-02-08 08:23:16 +0000

    updating model for booking sessions

--- ptproject/models.py
+++ ptproject/models.py
@@ -35,16 +35,17 @@ class Booking(models.Model):
     def __str__(self):
         return f'Booking by {self.name} on {self.date} at {self.time}'
 
+
 class Session(models.Model):
-    trainer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sessions')
+    trainer_name = models.CharField(max_length=100)
+    session_type = models.CharField(max_length=100)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    booked = models.BooleanField(default=False)
-    client = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='booked_sessions')
+    participant = models.ForeignKey(User, related_name='booked_sessions', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.trainer.username} session on {self.date} from {self.start_time} to {self.end_time}"
+        return f"{self.session_type} with {self.trainer_name} on {self.date} from {self.start_time} to {self.end_time}"
 
 
 class Contact(models.Model):
